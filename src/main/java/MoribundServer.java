ce5bc3bd58b2e76b420639ffcdd5f5f66fc4e6d6
aryouham <arhamjsiddiqui@gmail.com>
2018-11-23 10:46:39 -0600

    removed text chat test

--- src/main/java/MoribundServer.java
+++ src/main/java/MoribundServer.java
@@ -1,14 +1,11 @@
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Server;
-import net.TextListener;
-import net.packets.MessagePacket;
 
 import java.io.IOException;
 
 public class MoribundServer {
     public static void main(String[] args) {
         Server server = new Server();
-        server.addListener(new TextListener());
         registerPackets(server.getKryo());
 
         server.start();
@@ -21,6 +18,5 @@ public class MoribundServer {
     }
 
     private static void registerPackets(Kryo kryo) {
-        kryo.register(MessagePacket.class);
     }
 }
