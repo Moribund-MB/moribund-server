673ef56d7f9a5312d27d868629abdc8e57442594
Arham4 <arhamjsiddiqui@gmail.com>
2018-12-25 11:59:19 -0600

    kept key packets in use to allow for smoother animation
    Took 5 minutes

--- src/main/java/com/github/moribund/net/KeyListener.java
+++ src/main/java/com/github/moribund/net/KeyListener.java
@@ -2,6 +2,7 @@ package com.github.moribund.net;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+import com.github.moribund.MoribundServer;
 import com.github.moribund.net.packets.key.KeyPressedPacket;
 import com.github.moribund.net.packets.key.KeyPressedResponsePacket;
 import com.github.moribund.net.packets.key.KeyUnpressedPacket;
@@ -19,10 +20,12 @@ public class KeyListener extends Listener {
             val keyPressedPacket = (KeyPressedPacket) object;
             val keyPressedResponsePacket = new KeyPressedResponsePacket(keyPressedPacket.getPlayerId(),
                     keyPressedPacket.getKeyPressed());
+            MoribundServer.getInstance().sendPacketToEveryone(keyPressedResponsePacket);
         } else if (object instanceof KeyUnpressedPacket) {
             val keyUnpressedPacket = (KeyUnpressedPacket) object;
             val keyUnpressedResponsePacket = new KeyUnpressedResponsePacket(keyUnpressedPacket.getPlayerId(),
                     keyUnpressedPacket.getKeyUnpressed());
+            MoribundServer.getInstance().sendPacketToEveryone(keyUnpressedResponsePacket);
         }
     }
 }
