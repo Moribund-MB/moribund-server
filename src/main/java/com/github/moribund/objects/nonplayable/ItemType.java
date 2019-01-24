866e181bdebb10891c64cb9c54da760994074151
Arham4 <arhamjsiddiqui@gmail.com>
2019-01-23 23:32:49 -0600

    improved enums by using hashmaps to improve lookup time
    Took 6 minutes

--- src/main/java/com/github/moribund/objects/nonplayable/ItemType.java
+++ src/main/java/com/github/moribund/objects/nonplayable/ItemType.java
@@ -1,30 +1,35 @@
 package com.github.moribund.objects.nonplayable;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import lombok.Getter;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public enum ItemType {
-    ROCK(0), FEATHER(1), STICK(2), STRING(3), SPEAR(4), BOW(5), DART(6), ARROW(7);
+    ROCK(0), FEATHER(1), STICK(2), STRING(3), // items till this point are spawned via random()
+    SPEAR(4), BOW(5), DART(6), ARROW(7);
 
-    private static final ItemType[] VALUES = values();
+    private static final Int2ObjectMap<ItemType> VALUES;
     @Getter
     private final int id;
+
+    static {
+        VALUES = new Int2ObjectOpenHashMap<>();
+        for (ItemType itemType : values()) {
+            VALUES.put(itemType.getId(), itemType);
+        }
+    }
 
     ItemType(int id) {
         this.id = id;
     }
 
     public static ItemType random() {
-        return VALUES[ThreadLocalRandom.current().nextInt(STRING.ordinal() + 1)];
+        return VALUES.get(ThreadLocalRandom.current().nextInt(STRING.ordinal() + 1));
     }
 
     public static ItemType getItemType(int id) {
-        for (ItemType itemType : VALUES) {
-            if (itemType.id == id) {
-                return itemType;
-            }
-        }
-        return null;
+        return VALUES.get(id);
     }
 }
--- src/main/java/com/github/moribund/objects/nonplayable/ProjectileType.java
+++ src/main/java/com/github/moribund/objects/nonplayable/ProjectileType.java
@@ -1,24 +1,28 @@
 package com.github.moribund.objects.nonplayable;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import lombok.Getter;
 
 public enum ProjectileType {
     ARROW(0), DART(1), SPEAR(2);
 
-    private static final ProjectileType[] VALUES = values();
+    private static final Int2ObjectMap<ProjectileType> VALUES;
     @Getter
     private final int id;
+
+    static {
+        VALUES = new Int2ObjectOpenHashMap<>();
+        for (ProjectileType projectileType : values()) {
+            VALUES.put(projectileType.getId(), projectileType);
+        }
+    }
 
     ProjectileType(int id) {
         this.id = id;
     }
 
     public static ProjectileType getForId(int id) {
-        for (ProjectileType projectileType : VALUES) {
-            if (projectileType.id == id) {
-                return projectileType;
-            }
-        }
-        return null;
+        return VALUES.get(id);
     }
 }
